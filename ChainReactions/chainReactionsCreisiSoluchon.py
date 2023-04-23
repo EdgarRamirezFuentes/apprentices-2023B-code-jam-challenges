76ffffdd95830657c050b3bd69312de0824eac1a
Edgar Ramirez <edgar.ramirez.fuentes.dev@gmail.com>
2023-04-23 13:43:47 -0600

    Lo hicimos! We did it! Dijo Dora la exploradora.

--- ChainReactions/chainReactionsCreisiSoluchon.py
+++ ChainReactions/chainReactionsCreisiSoluchon.py
@@ -1,64 +1,81 @@
-"""
-Es una solución que apenas voy a probar. La neta, yo creo que va a tirar TLE, pero 
-esto es con fé y confiando en el corazón de las cartas.
-No es mucho, pero es trabajo honesto.
+import math
+import sys
 
-UPDATE: Está mal este approach, se me olvidó considerar el caso donde la cadena se interrumpe por nodo visitado 
-y se suma ese fun fact. :c 
-Voy a echarle otra pensada al problema. ggg
-"""
+sys.setrecursionlimit(10**8)
+
+output = 0
 
 def get_max_fun_factor(module_chain : dict, current_module : int, roots : list):
-    module = module_chain[current_module]
-    max_fun_factor = module["fun_factor"]
+    """Get the max fun function of the current node
+    
+    Args:
+        module_chain: A dictionary, which contains all the modules in the forest.
+        current_module: The index of the current module
+        roots: A list, which contains the index of the root nodes (Point to the abyss)
 
+    
+    Returns:
+        The max fun factor in the current node.
+    """
+
+    # Store the total fun factor in the forest.
+    global output
+
+    module = module_chain[current_module]
+
+    # Store the max fun factor in the current node
+    max_fun_factor = module["max_fun_factor"]
+
+
+    """ In order to get the max fun fact in the forest,
+    for each node that has more than 1 children, we're 
+    gonna follow the path with the smaallest one, and 
+    we're gonna add up the others to the total fun factor
+    in order to increase the sum.
+    """
+    smallest_fun_factor = math.inf
+    children_fun_factor_sum = 0
+    
     if not module["children"]: 
-        module["max_fun_factor"] = max_fun_factor
         return max_fun_factor
 
     for child in module["children"]:
         child_fun_factor = get_max_fun_factor(module_chain, child, roots)
-        max_fun_factor = max(max_fun_factor, child_fun_factor)
+        children_fun_factor_sum += child_fun_factor
+        smallest_fun_factor = min(smallest_fun_factor, child_fun_factor)
+    
+    output += children_fun_factor_sum - smallest_fun_factor
 
-    module["max_fun_factor"] = max(max_fun_factor, module["max_fun_factor"])
-
-    if current_module in roots:
-        smallest_fun_factor = module["fun_factor"]
-        max_fun_factor = 0
-        for child in module["children"]:
-            child_fun_factor = module_chain[child]["max_fun_factor"]
-            smallest_fun_factor = min(smallest_fun_factor, child_fun_factor)
-            max_fun_factor += child_fun_factor
-
-        if smallest_fun_factor < module["fun_factor"]: max_fun_factor += module["fun_factor"] - smallest_fun_factor
+    module["max_fun_factor"] = max(smallest_fun_factor, max_fun_factor)
+    max_fun_factor = module["max_fun_factor"]
 
     return max_fun_factor
-        
+
+
 
 if __name__ == "__main__":
     test_cases = int(input())
 
     for _ in range(test_cases):
         modules = int(input())
-
         fun_factors = [int(x) for x in input().split()]
         pointing = [int(x) for x in input().split()]
         module_chain = {}
         roots = set()
+        output = 0
 
         for module in range(modules):
             module_chain[module + 1] = {
                 "fun_factor" : fun_factors[module],
                 "children" : [], 
-                "max_fun_factor" : 0,
+                "max_fun_factor" : fun_factors[module],
             }
 
             if pointing[module] == 0: roots.add(module + 1)
             else: module_chain[pointing[module]]["children"].append(module  + 1)
         
-        max_fun_factor = 0
-        
         for root in roots:
-            max_fun_factor += get_max_fun_factor(module_chain, root, roots)
+            root_fun_factor = get_max_fun_factor(module_chain, root, roots)
+            output += root_fun_factor
 
-        print(f"Case #{_ + 1} : {max_fun_factor}")
+        print(f"Case #{_ + 1}: {output}")
